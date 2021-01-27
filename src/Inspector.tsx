43c0595ff7700beb49bede13b8a76d5486ce5734
Dom Crayford <domcrayford@gmail.com>
2021-01-27 10:28:31 +0000

    Style inspector

--- src/Inspector.tsx
+++ src/Inspector.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent, ReactElement, useEffect, useState } from 'react';
+import React, { FunctionComponent, useCallback, useState } from 'react';
 import { formatBinary, formatHex } from './formatters';
+import './Inspector.css';
 
 interface InspectorProps {
   className: string;
@@ -9,37 +10,36 @@ interface InspectorProps {
 }
 
 const Inspector: FunctionComponent<InspectorProps> = ({ className, data, cursor, onSplitBlock }) => {
+  const [little, setLittle] = useState(false);
+
   const byte = data.getUint8(cursor);
 
-  const word = cursor + 1 < data.byteLength ? data.getUint16(cursor) : byte;
-  const dword = cursor + 3 < data.byteLength ? data.getUint32(cursor) : byte;
+  const toggleLittle = useCallback(() => {
+    setLittle(!little);
+  }, [little]);
 
-  const wordl = cursor + 1 < data.byteLength ? data.getUint16(cursor, true) : byte;
-  const dwordl = cursor + 3 < data.byteLength ? data.getUint32(cursor, true) : byte;
+  const word = cursor + 1 < data.byteLength ? data.getUint16(cursor, little) : byte;
+  const dword = cursor + 3 < data.byteLength ? data.getUint32(cursor, little) : byte;
 
   return (
     <div className={className}>
       <div className="InspectorTitle">{formatHex(cursor, 16)}</div>
       <button onClick={onSplitBlock}>Split</button>
+      <div>
+        <input type="checkbox" checked={little} onChange={toggleLittle} />
+        Little Endian
+      </div>
       <div className="InspectorRow">
         <div className="InspectorHeader">Binary</div>
         <div>{formatBinary(byte, 8)}</div>
-        <div className="InspectorType">Big-endian</div>
         <div>{formatBinary(word, 16)}</div>
         <div>{formatBinary(dword, 32)}</div>
-        <div className="InspectorType">Little-endian</div>
-        <div>{formatBinary(wordl, 16)}</div>
-        <div>{formatBinary(dwordl, 32)}</div>
       </div>
       <div className="InspectorRow">
         <div className="InspectorHeader">Hex</div>
         <div>{formatHex(byte, 2)}</div>
-        <div className="InspectorType">Big-endian</div>
         <div>{formatHex(word, 4)}</div>
         <div>{formatHex(dword, 8)}</div>
-        <div className="InspectorType">Little-endian</div>
-        <div>{formatHex(wordl, 4)}</div>
-        <div>{formatHex(dwordl, 8)}</div>
       </div>
     </div>
   );
